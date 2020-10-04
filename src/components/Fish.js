b76c4ecea7c4580147db7525a1fbded38846bde6
AdnCodez <adnan@adncodez.com>
2020-10-04 23:31:44 +0100

    added prop type check

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import {formatPrice} from '../helpers'
 
 
@@ -6,6 +7,16 @@ class Fish extends React.Component {
     // handleClick = () => {
     //     this.props.addToOrder(this.props.index)
     // }
+    static propTypes = {
+        data: PropTypes.shape({
+            name: PropTypes.string,
+            image: PropTypes.string,
+            desc: PropTypes.string,
+            status: PropTypes.string,
+            price: PropTypes.number
+        }),
+        addToOrder: PropTypes.func
+    }
     render() {
         const { name, image, price, desc, status } = this.props.data;
         const isAvailable = status === "available";
